675d7c18d6da553f2534c3ee0bb106a5cc66c6e0
NielsGaljaard <galjaard.niels@gmail.com>
2022-06-04 08:12:08 +0200

    removed hotkey help for other applications

--- rc.lua
+++ rc.lua
@@ -16,7 +16,6 @@ local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
-require("awful.hotkeys_popup.keys")
 
 -- Load Debian menu entries
 
